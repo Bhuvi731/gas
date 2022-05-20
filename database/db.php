3d1f180e629d702da342336d300b6233e13ca858
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-05-20 15:16:16 +0530

    Update db.php

--- database/db.php
+++ database/db.php
@@ -3,7 +3,7 @@
 
 $port        = "port = 5432";
    $dbname      = "dbname = gashub";
-   $credentials = "user =gashubadmin";
+   $credentials = "user =gashubadmin password=123456";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
