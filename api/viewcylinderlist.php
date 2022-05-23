86ca18139e609eec00dbf0d18df704eecf21ef6c
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-05-23 17:57:33 +0530

    Add files via upload

---
+++ api/viewcylinderlist.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: text/plain');
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+include_once '../database/db.php';
+ 
+    $sql=pg_query($db,"SELECT cylindertype.type,cylinderweight.weight,cylindertype.status,cylindertype.createdby FROM cylinderweight LEFT JOIN cylindertype ON cylindertype.id = cylinderweight.id WHERE cylindertype.status=1");
+   
+        if($sql2=pg_fetch_assoc($sql))
+        {
+             http_response_code(201);         
+             echo json_encode(array("message" => "Successfull"));
+        }else{
+    http_response_code(400);    
+    echo json_encode(array("message" => "Error Please Check."));
+               }
+?>
