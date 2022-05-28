f04c292a2f6056ff944682b93f620a945d82dcbb
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-05-28 17:37:28 +0530

    Update viewcylinderlist.php

--- api/viewcylinderlist.php
+++ api/viewcylinderlist.php
@@ -6,14 +6,18 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
- 
-    $sql=pg_query($db,"SELECT cylindertype.type,cylinderweight.weight,cylindertype.status,cylindertype.createdby FROM cylinderweight LEFT JOIN cylindertype ON cylindertype.id = cylinderweight.id WHERE cylindertype.status=1");
-   
+
+$sql=pg_query($db,"SELECT cylindertype.type,cylinderweight.weight,cylindertype.status,cylindertype.createdby FROM cylinderweight LEFT JOIN cylindertype ON cylindertype.id = cylinderweight.id WHERE cylindertype.status=1");
+ if($sql){
         if($sql2=pg_fetch_assoc($sql))
         {
              http_response_code(201);         
              echo json_encode(array("message" => "Successfull"));
         }else{
+          http_response_code(503);        
+        echo json_encode(array("message" => "Error"));
+             }
+    }else{
     http_response_code(400);    
     echo json_encode(array("message" => "Error Please Check."));
                }
