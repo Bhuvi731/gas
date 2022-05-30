381290e81839c1e75b594d8151122de161dd704d
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-05-30 16:22:47 +0530

    Update viewcylinderlist.php

--- api/viewcylinderlist.php
+++ api/viewcylinderlist.php
@@ -7,8 +7,10 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
 
+
 $sql=pg_query($db,"SELECT * FROM products LEFT JOIN cylindertype on cylindertype.id = products.cylindertypeid left join cylinderweight on cylinderweight.id =products.cylinderweightid left join vendors on vendors.id = products.vendorid where products.status=1");
- if($sql){
+ echo"SELECT * FROM products LEFT JOIN cylindertype on cylindertype.id = products.cylindertypeid left join cylinderweight on cylinderweight.id =products.cylinderweightid left join vendors on vendors.id = products.vendorid where products.status=1";
+if($sql){
       while($sql2=pg_fetch_assoc($sql)){
 
              http_response_code(201);         
