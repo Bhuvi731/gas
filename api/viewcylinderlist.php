04b9aad68cfe9e16074c1cbbdf3c2ae6b3fe0a52
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-01 12:34:27 +0530

    Update viewcylinderlist.php

--- api/viewcylinderlist.php
+++ api/viewcylinderlist.php
@@ -7,31 +7,17 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
 
-// $sql=pg_query($db,"SELECT * FROM cylinderweight LEFT JOIN cylindertype ON cylindertype.id = cylinderweight.id WHERE cylindertype.status=1");
-
-//$location=$_POST("location");
-// $businesstype=$_POST['businesstype'];
-// if(!empty($businesstype)){
-// if($businesstype==2){
-//   $where="where businesstype=2";
-// }
-// else($businesstype==2){
-
-//   $where="where businesstype=2";
-
-//   }
-
 $sql=pg_query($db,"SELECT * FROM products LEFT JOIN  cylindertype on cylindertype.id = products.cylindertypeid left join cylinderweight on cylinderweight.id =products.cylinderweightid left join vendors on vendors.id = products.vendorid where products.status=1");
 $my=array();
  if($sql){
    //var_dump($sql); 
     while($sql2=pg_fetch_assoc($sql)){
  
-             http_response_code(201);       
-            
-            $my[]=$sql2;
-           }
-           echo json_encode($my);
+             http_response_code(201);
+                 $my[]=$sql2;
+                 
+             }      
+                echo json_encode($my);
            }else{
           http_response_code(503);        
         echo json_encode(array("message" => "Error"));
